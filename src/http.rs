519c4231bfe5d34c6fa112130f8e8f9d15526180
Cameron Zollinger <cameron@zolly.dev>
2023-02-08 18:41:39 -0700

    temporary change

--- src/http.rs
+++ src/http.rs
@@ -1,8 +1,38 @@
 use std::collections::HashMap;
-use std::time::Instant;
+use std::time::{Instant, Duration};
 use reqwest;
+use reqwest::header::{COOKIE};
+use serde::{Serialize, Deserialize};
 
-pub fn make_request(report_name: &String, api_key: &String, base_url: &String) -> Result<Vec<u128>, reqwest::Error> {
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct RequestBody {
+    report_view: ReportView
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct ReportView {
+    filter: Filter,
+    report_id: u32,
+    time_zone: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct Filter {
+    is_utc_time: bool,
+    user_time_range_option: u32,
+    table_args: TableArgs
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct TableArgs {
+    columns_to_include: Vec<String>
+}
+
+pub fn report_api_request(report_name: &String, api_key: &String, base_url: &String) -> Result<Vec<u128>, reqwest::Error> {
     let client = reqwest::blocking::Client::new();
     let url = format!("{}/api/report-api?apiKey={}", base_url, api_key);
     let start_date = String::from("2022-10-01-00:00");
@@ -32,6 +62,64 @@ pub fn make_request(report_name: &String, api_key: &String, base_url: &String) -
             println!("{:#?}", elapsed_time)
         } else {
             println!("FAIL");
+        }
+
+        response_times.push(elapsed_time.as_millis());
+    }
+
+    Ok(response_times)
+}
+
+pub fn custom_request(url: &String, session_id: &String) -> Result<Vec<u128>, reqwest::Error> {
+    let client = reqwest::blocking::Client::new();
+    let report_view = r#"{
+        "reportView": {
+          "filter": {
+            "filterOptions": [],
+            "isUtcTime": true,
+            "tableArgs": {
+              "columnsToInclude": [
+                "DATE",
+                "FROM FULL NAME",
+                "TO FULL NAME",
+                "POINTS AWARDED",
+                "PROGRAM NAME",
+                "TAGS"
+              ]
+            },
+            "userTimeRangeOption": 11
+          },
+          "reportId": 5,
+          "timeZone": "UTC"
+        }
+      }"#;
+    let body: RequestBody = serde_json::from_str(&report_view).unwrap();
+    let cookie_value = ".ASPXAUTH=".to_owned() + session_id;
+    let mut response_times = Vec::new();
+
+    for _i in 1..10 {
+        let cookie_value = cookie_value.clone();
+        let anti_forgery_token = "9p8N4aNdFXbj75aBMrQO5wqvDVY1GjBD4ppjUZPITCbwhcJj4rLOPKdjHCRVWnSPG00gkbpRSHZPkGVl591E9Zk1xj54ZpmAG7ar3M-zeqY1:I5fkHKXQlU7u8xuUn3AToXu_E3kigz8Buqh56-PowHlzG7m9HxVsWGB6J3L7XHc80YAnUS44ex8wyRQPAiE4gZxpWIaMubSyAcj4WW17F0w1";
+        let now = Instant::now();
+        let res = client
+            .post(url.to_string())
+            .timeout(Duration::from_secs(240))
+            .json(&body)
+            .header(COOKIE, cookie_value)
+            .header("X-Anti-Forgery-Token", anti_forgery_token)
+            .send()?;
+
+        let elapsed_time = now.elapsed();
+
+        // let res = req.send()
+        //     .await?
+        //     .json::<Vec<HashMap<String, String>>>()
+        //     .await?;
+
+        if let reqwest::StatusCode::OK = res.status() {
+            println!("{:#?}", elapsed_time)
+        } else {
+            println!("FAIL, status code: {}, url: {}, message: {:#?}", res.status(), url, res.text());
         }
 
         response_times.push(elapsed_time.as_millis());
--- src/main.rs
+++ src/main.rs
@@ -8,78 +8,92 @@ mod http;
 mod utils;
 
 struct Config {
-    base_url: String,
-    api_key: String,
-    report_name: String,
+    url: String,
+    session_id: String
 }
 
 impl Config {
     fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
+        if args.len() < 2 {
             return Err("not enough arguments");
         }
 
-        let base_url = args[1].clone();
-        let api_key = args[2].clone();
-        let report_name = args[3].clone();
+        let url = args[1].clone();
+        let session_id = args[2].clone();
 
-        Ok(Config { base_url, api_key, report_name })
+        Ok(Config { url, session_id })
     }
 }
 
-fn main() {
+fn main() -> Result<(), reqwest::Error> {
     let args: Vec<String> = env::args().collect();
     
-    if args.len() > 0 {
+    // if args.len() > 0 {
         let config = Config::build(&args).unwrap_or_else(|err| {
             println!("Problem parsing arguments: {err}");
             process::exit(1);
         });
-        run(config);
-    } else {
-        inquire::set_global_render_config(cli::get_render_config());
-        run_cli();
-    }
+        
+        let (tx, rx) = mpsc::channel();
+        for _ in 1..10 {
+            let tx = tx.clone();
+            let url = config.url.clone();
+            let session_id = config.session_id.clone();
+            thread::spawn(move || {
+                let times = http::custom_request(&url, &session_id);
+                tx.send(times).unwrap();
+            });
+        }
+    
+        let mut received = rx.recv().unwrap()?;
+        println!("MEAN: {}ms", utils::average(&received));
+        println!("MEDIAN: {}ms", utils::median(&mut received));
+    
+        Ok(())
+    // } else {
+    //     inquire::set_global_render_config(cli::get_render_config());
+    //     run_cli();
+    // }
 }
 
-fn run(config: Config) -> Result<(), reqwest::Error> {
-    let (tx, rx) = mpsc::channel();
-    for _ in 1..10 {
-        let tx = tx.clone();
-        let base_url = config.base_url.clone();
-        let api_key = config.api_key.clone();
-        let report_name = config.report_name.clone();
-        thread::spawn(move || {
-            let times = http::make_request(&report_name, &api_key, &base_url);
-            tx.send(times).unwrap();
-        });
-    }
+// fn run(config: Config) -> Result<(), reqwest::Error> {
+//     let (tx, rx) = mpsc::channel();
+//     for _ in 1..10 {
+//         let tx = tx.clone();
+//         let base_url = config.base_url.clone();
+//         let api_key = config.api_key.clone();
+//         let report_name = config.report_name.clone();
+//         thread::spawn(move || {
+//             let times = http::make_request(&report_name, &api_key, &base_url);
+//             tx.send(times).unwrap();
+//         });
+//     }
 
-    let mut received = rx.recv().unwrap()?;
-    println!("MEAN: {}ms", utils::average(&received));
-    println!("MEDIAN: {}ms", utils::median(&mut received));
-    println!("MODE: {}ms", utils::mode(&received));
+//     let mut received = rx.recv().unwrap()?;
+//     println!("MEAN: {}ms", utils::average(&received));
+//     println!("MEDIAN: {}ms", utils::median(&mut received));
+//     println!("MODE: {}ms", utils::mode(&received));
 
-    Ok(())
-}
+//     Ok(())
+// }
 
-fn run_cli() -> Result<(), reqwest::Error> {
-    let input = cli::get_user_input();
-    println!("\nGetting {} report for this month at {}...", &input["report_name"], &input["base_url"]);
-    let (tx, rx) = mpsc::channel();
-    for _ in 1..10 {
-        let tx = tx.clone();
-        let input = input.clone();
-        thread::spawn(move || {
-            let times = http::make_request(&input["report_name"], &input["api_key"], &input["base_url"]);
-            tx.send(times).unwrap();
-        });
-    }
+// fn run_cli() -> Result<(), reqwest::Error> {
+//     let input = cli::get_user_input();
+//     println!("\nGetting {} report for this month at {}...", &input["report_name"], &input["base_url"]);
+//     let (tx, rx) = mpsc::channel();
+//     for _ in 1..10 {
+//         let tx = tx.clone();
+//         let input = input.clone();
+//         thread::spawn(move || {
+//             let times = http::make_request(&input["report_name"], &input["api_key"], &input["base_url"]);
+//             tx.send(times).unwrap();
+//         });
+//     }
 
-    let mut received = rx.recv().unwrap()?;
-    println!("MEAN: {}ms", utils::average(&received));
-    println!("MEDIAN: {}ms", utils::median(&mut received));
-    println!("MODE: {}ms", utils::mode(&received));
+//     let mut received = rx.recv().unwrap()?;
+//     println!("MEAN: {}ms", utils::average(&received));
+//     println!("MEDIAN: {}ms", utils::median(&mut received));
+//     println!("MODE: {}ms", utils::mode(&received));
 
-    Ok(())
-}
+//     Ok(())
+// }
